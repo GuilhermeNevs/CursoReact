cf6d401d03b54957a561c42db1f989ed60498088
Guilherme Neves <guilhermenf03@gmail.com>
2024-03-28 21:06:40 -0300

    Criação primeiro componente
    Criei meu primeiro componente e importei para o meu arquivo principal

--- meu-projeto/src/App.js
+++ meu-projeto/src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
 
+import FirstComponent from './components/FirstComponent';
+
 function App() {
   return (
     <div className="App">
         <h1>Olá, React</h1>
+        <FirstComponent/>
     </div>
+    
   );
 }
 
